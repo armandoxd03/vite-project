8b016fcf3602cef4589e2a398a03d77fbd01b4ee
armandoxd03 <johnroyducut04@gmail.com>
2025-02-06 18:40:47 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,9 @@ function App() {
   return (
     <div>
       <h1 style={{ color: 'black' }}>John Roy Ducut</h1>
-      <div id="Container"></div>
+      <div id="Container"><p>CPEITEL</p></div>
     </div>
-  );
+    );
 }
 
 export default App;
