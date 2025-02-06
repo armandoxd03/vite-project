0610f7e6de22346385b6c112627ce1ce2a1af255
armandoxd03 <johnroyducut04@gmail.com>
2025-02-06 17:42:19 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,30 +2,10 @@ import './App.css';
 
 function App() {
   return (
-    <section>
-      <h1 style={{color: 'black'}}>John Roy Ducut</h1>
-      <Container />
-    </section>
-  );
-}
-
-function Container() {
-  return (
-    <section style={{ textAlign: 'center' }}>
-      <div style={{
-        margin: '0 auto',
-        width: '300px',
-        height: '300px',
-        border: '5px solid rgb(58, 0, 0)',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: '#ffa8a8',
-        color: 'black'
-      }}>
-        CPEITEL
-      </div>
-    </section>
+    <div>
+      <h1 style={{ color: 'black' }}>John Roy Ducut</h1>
+      <div id="Container"></div>
+    </div>
   );
 }
 
